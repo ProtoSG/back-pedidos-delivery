18d727592d7f509e16eaa7e8a19d21395e713be0
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-05-06 01:19:45 -0500

    add port

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from config import confi
 from src import init_app
 from flask_jwt_extended import JWTManager
 
+from decouple import config
 
 configuration = confi['development']
+port = int(config('PORT', default=4000))
 app = init_app(configuration)
 jwt = JWTManager(app)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port = port)
