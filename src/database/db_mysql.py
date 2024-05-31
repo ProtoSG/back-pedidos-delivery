d81d06e566a0b73e65930ffc1f963f727da4a86a
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-05-31 11:45:18 -0500

    feature config

--- src/database/db_mysql.py
+++ src/database/db_mysql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 import os
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,9 +10,11 @@ def get_connection():
         TURSO_DATABASE_URL = os.environ.get("TURSO_DATABASE_URL")
         TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
 
-        dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
-        engine = create_engine(dbUrl, connect_args={'check_same_thread': False}, echo=True)
-        session = Session(engine)
+        dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}"
+        engine = create_engine(dbUrl, echo=True)
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
         print("Obteniendo session")
         if(session):
             print("Si hay")
