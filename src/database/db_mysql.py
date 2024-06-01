b47e507badbd61edc7d0d9c44895b9a84356f38f
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-06-01 10:42:25 -0500

    f db

--- src/database/db_mysql.py
+++ src/database/db_mysql.py
@@ -6,19 +6,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
-    try:
-        TURSO_DATABASE_URL = os.environ.get("TURSO_DATABASE_URL")
-        TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
+    # try:
+    TURSO_DATABASE_URL = os.environ.get("TURSO_DATABASE_URL")
+    TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
 
-        dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}"
-        engine = create_engine(dbUrl, echo=True)
+    dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}"
+    engine = create_engine(dbUrl, echo=True)
 
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        print("Obteniendo session")
-        if(session):
-            print("Si hay")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    print("Obteniendo session")
+    if(session):
+        print("Si hay")
 
-        return session
-    except Exception as ex:
-        return None
+    return session
+    # except Exception as ex:
+    #     return None
