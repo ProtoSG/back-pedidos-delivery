3a06797e375624fd3e8f74c55f2c8188fab463f0
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-03 16:40:44 -0500

    fix: read enviroment variables

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
 
 class Config():
+    load_dotenv()
     SECRET_KEY = os.environ.get('SECRET_KEY')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
