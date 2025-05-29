69a19081c21301a597874b7972cd27a003b97b5f
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-28 21:28:46 -0500

    fix: delete init JWTManager

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from config import confi
 from src import init_app
-from flask_jwt_extended import JWTManager
-
 
 configuration = confi['development']
 app = init_app(configuration)
-jwt = JWTManager(app)
 
 if __name__=='__main__':
     app.run()
