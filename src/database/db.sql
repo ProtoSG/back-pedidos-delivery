5f5d327340f5d48c84ae760eba5b207d9eb6131d
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-28 21:23:28 -0500

    feat: update db.sql

--- src/database/db.sql
+++ src/database/db.sql
@@ -6,54 +6,54 @@ DROP TABLE if EXISTS Producto;
 DROP TABLE if EXISTS Categoria;
 DROP TABLE if EXISTS Admin;
 
-CREATE TABLE Admin(
+CREATE TABLE Admin (
     admin_id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
     password TEXT NOT NULL
 );
 
-CREATE TABLE Categoria(
-    categoria_id INTEGER  PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT
+CREATE TABLE Categoria (
+    categoria_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL
 );
 
-CREATE TABLE Producto(
+CREATE TABLE Producto (
     producto_id INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT NOT NULL,
-    categoria_id INTEGER NOT NULL,
+    categoria_id INTEGER,
     precio REAL NOT NULL,
     descripcion TEXT,
     imagen_url TEXT,
-    FOREIGN KEY (categoria_id) REFERENCES Categoria(categoria_id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (categoria_id) REFERENCES Categoria(categoria_id)
 );
 
-CREATE TABLE Extra(
+CREATE TABLE Extra (
     extra_id INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT NOT NULL,
     precio REAL NOT NULL,
     imagen_url TEXT
 );
 
-CREATE TABLE Pedido(
+CREATE TABLE Pedido (
     pedido_id INTEGER PRIMARY KEY AUTOINCREMENT,
     total REAL,
     fecha_hora DATETIME
 );
 
-CREATE TABLE Pedido_Producto(
+CREATE TABLE Pedido_Producto (
     pedido_id INTEGER,
     producto_id INTEGER,
     cantidad INTEGER,
     sub_total REAL,
-    FOREIGN KEY (pedido_id) REFERENCES Pedido(pedido_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (pedido_id) REFERENCES Pedido(pedido_id),
     FOREIGN KEY (producto_id) REFERENCES Producto(producto_id)
 );
 
-CREATE TABLE Pedido_Extra(
+CREATE TABLE Pedido_Extra (
     pedido_id INTEGER,
     extra_id INTEGER,
     cantidad INTEGER,
     sub_total REAL,
-    FOREIGN KEY (pedido_id) REFERENCES Pedido(pedido_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (extra_id) REFERENCES Extra(extra_id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (pedido_id) REFERENCES Pedido(pedido_id),
+    FOREIGN KEY (extra_id) REFERENCES Extra(extra_id)
 );
