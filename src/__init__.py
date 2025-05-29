453184c600fecc1cf7bd3eb91f3c7f20cc25899a
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-28 21:27:17 -0500

    feat: init Bcrypt and JWTManager

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended.jwt_manager import JWTManager
 from .routes import categoria, extra, producto, admin, login, pedido, pedido_producto, pedido_extra
 from flask_cors import CORS
 
 def init_app(config):
     app = Flask(__name__)
-
+    Bcrypt(app)
+    JWTManager(app)
 
     # Configuracion
     app.config.from_object(config)
-    
+
     CORS(
-        app, 
+        app,
         origins=[app.config["ORIGIN_URL"]],
         supports_credentials=True
     )
